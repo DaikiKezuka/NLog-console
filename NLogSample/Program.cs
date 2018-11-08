12cff65b8f2dac3424fb32b7afebb9dc532d0b3b
d-kezuka <d-kezuka@e-reverse.com>
2018-11-08 17:33:57 +0900

    New!

---
+++ NLogSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using System;
+
+namespace NLogSample
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Factory.Configuration.Variables.Add("runtime", "string文字列をセットします。");
+            logger.Factory.ReconfigExistingLoggers();
+
+
+            logger.Info("===== Program Info Start =====");
+            logger.Info("===== Program Info End =====");
+
+            logger.Warn("===== Program Warn Start =====");
+            logger.Warn("===== Program Warn End =====");
+
+            logger.Error("===== Program Error Start =====");
+            logger.Error("===== Program Error End =====");
+
+            Console.ReadKey();
+        }
+    }
+}
